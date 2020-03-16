4f62f0d313c959b411cedde7a9823d8e712edeaf
abhay-98 <abhay.806.98@gmail.com>
2020-03-16 05:35:03 +0530

    showing multiple dependency injection without setter method

--- src/main/java/employee/Employee.java
+++ src/main/java/employee/Employee.java
@@ -4,42 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 public class Employee {
+
+	@Value("${employee.name}")
 	private String name;
+
+	@Value("${employee.id}")
 	private int id;
+
+	@Value("${employee.gender}")
 	private String gender;
+
+	@Autowired
 	private Salaray salaray;
 
 	public String getName() {
 		return name;
 	}
-	@Value("${employee.name}")
-	public void setName(String name) {
-		this.name = name;
-	}
+	
 	public int getId() {
 		return id;
-	}
-	@Value("${employee.id}")
-	public void setId(int id) {
-		this.id = id;
 	}
 	
 	public String getGender() {
 		return gender;
 	}
-	@Value("${employee.gender}")
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
 
 	public void getSalaray() {
 		 salaray.moneyAllotted();
 	}
-@Autowired
-	public void setSalaray(Salaray salaray) {
-		this.salaray = salaray;
-	}
-	
-	
-
 }
